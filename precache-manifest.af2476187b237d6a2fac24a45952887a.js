c21b54f1ec3c9b4c0e5f3ad4aee93119c5ad0dbf
Sohail Haider <sohailh343@gmail.com>
2020-10-22 12:55:55 +0500

    Updates

---
+++ precache-manifest.af2476187b237d6a2fac24a45952887a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3129b01f5fd7399e2f7af0346646fe42",
+    "url": "/react-pages/index.html"
+  },
+  {
+    "revision": "d88f0f633f641fe641f5",
+    "url": "/react-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "656411646c4b2524a14a",
+    "url": "/react-pages/static/js/2.550ada4c.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/react-pages/static/js/2.550ada4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d88f0f633f641fe641f5",
+    "url": "/react-pages/static/js/main.aa3149d4.chunk.js"
+  },
+  {
+    "revision": "088ad8a5a51d09105d11",
+    "url": "/react-pages/static/js/runtime-main.8d234e1d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
